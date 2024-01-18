82a9d8e0582952df0e91dde518c436bd30cc3845
Arpad Borsos <arpad.borsos@sentry.io>
2024-01-18 17:49:46 +0100

    Reimplement it all based on much more unsafe code :-)

---
+++ src/buf24.rs
@@ -0,0 +1,200 @@
+use core::num::NonZeroU8;
+use core::slice;
+use core::{mem, ops};
+
+use alloc::sync::Arc;
+
+#[repr(transparent)]
+pub struct Buf24(Buf24Inner);
+
+pub(crate) const INLINE_CAP: usize = 23;
+const PADDING_BYTES: usize = 7;
+
+#[repr(C)]
+#[derive(Clone, Copy, PartialEq)]
+struct Buf24Inner {
+    tag: NonZeroU8,
+    _padding: [u8; PADDING_BYTES],
+    ptr: u64,
+    len: u64,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct Buf24Inline {
+    tag_and_len: u8,
+    buf: [u8; INLINE_CAP],
+}
+
+const _: () = {
+    assert!(mem::size_of::<Buf24>() == 24);
+    assert!(mem::size_of::<Option<Buf24>>() == 24);
+
+    assert!(mem::size_of::<Buf24Inline>() == mem::size_of::<Buf24Inner>());
+};
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Tag {
+    Inline = 0b001,
+    Arc = 0b010,
+    Static = 0b100,
+}
+const TAG_SHIFT: usize = 3;
+const TAG_MASK: u8 = 0b111;
+
+impl Buf24 {
+    /// Constructs inline variant of `Buf24`.
+    ///
+    /// Panics if `input.len() > 23`.
+    #[inline]
+    pub const fn new_inline(input: &[u8]) -> Self {
+        let len = input.len();
+        assert!(len <= INLINE_CAP); // avoids checks in loop
+
+        let mut buf = [0; INLINE_CAP];
+
+        let mut i = 0;
+        while i < len {
+            buf[i] = input[i];
+            i += 1
+        }
+
+        let tag_and_len = ((len as u8) << TAG_SHIFT) | Tag::Inline as u8;
+        unsafe { mem::transmute(Buf24Inline { tag_and_len, buf }) }
+    }
+
+    #[inline]
+    pub fn new_static(input: &'static [u8]) -> Self {
+        let len = input.len();
+        if len <= INLINE_CAP {
+            Self::new_inline(input)
+        } else {
+            let ptr = input.as_ptr() as usize as u64;
+            let tag = unsafe { NonZeroU8::new_unchecked(Tag::Static as u8) };
+            Self(Buf24Inner {
+                tag,
+                _padding: [0; PADDING_BYTES],
+                ptr,
+                len: len as u64,
+            })
+        }
+    }
+
+    pub fn new(input: &[u8]) -> Self {
+        let len = input.len();
+        if len <= INLINE_CAP {
+            Self::new_inline(input)
+        } else {
+            let arc = Arc::from(input);
+            Self::from_arc(arc)
+        }
+    }
+
+    #[inline]
+    pub(crate) fn from_arc(arc: Arc<[u8]>) -> Self {
+        let len = arc.len();
+        if len <= INLINE_CAP {
+            return Self::new_inline(&arc);
+        }
+
+        let ptr = Arc::into_raw(arc) as *const u8 as usize as u64;
+        let tag = unsafe { NonZeroU8::new_unchecked(Tag::Arc as u8) };
+        Self(Buf24Inner {
+            tag,
+            _padding: [0; PADDING_BYTES],
+            ptr,
+            len: len as u64,
+        })
+    }
+
+    #[inline]
+    pub(crate) fn as_arc(&self) -> Option<Arc<[u8]>> {
+        if self.tag() != Tag::Arc {
+            return None;
+        }
+
+        let arc_ptr = self.as_bytes() as *const [u8];
+
+        Some(unsafe { Arc::from_raw(arc_ptr) })
+    }
+
+    #[inline(always)]
+    fn tag(&self) -> Tag {
+        unsafe { mem::transmute(self.0.tag.get() & TAG_MASK) }
+    }
+
+    #[inline(always)]
+    pub fn len(&self) -> usize {
+        if self.tag() == Tag::Inline {
+            (self.0.tag.get() >> TAG_SHIFT) as usize
+        } else {
+            self.0.len as usize
+        }
+    }
+
+    #[inline(always)]
+    pub fn is_heap_allocated(&self) -> bool {
+        self.tag() == Tag::Arc
+    }
+
+    #[inline(always)]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    #[inline(always)]
+    pub fn as_bytes(&self) -> &[u8] {
+        let len = self.len();
+
+        if self.tag() == Tag::Inline {
+            let inline: &Buf24Inline = unsafe { mem::transmute(self) };
+            return &inline.buf[..len];
+        }
+
+        let data = self.0.ptr as usize as *const u8;
+        unsafe { slice::from_raw_parts(data, len) }
+    }
+}
+
+impl Drop for Buf24 {
+    fn drop(&mut self) {
+        drop(self.as_arc());
+    }
+}
+
+impl Clone for Buf24 {
+    fn clone(&self) -> Self {
+        if let Some(arc) = self.as_arc() {
+            unsafe { Arc::increment_strong_count(Arc::into_raw(arc)) };
+        }
+
+        Self(self.0)
+    }
+}
+
+impl PartialEq for Buf24 {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0 || self.as_bytes() == other.as_bytes()
+    }
+}
+
+impl Eq for Buf24 {}
+
+impl Default for Buf24 {
+    #[inline(always)]
+    fn default() -> Self {
+        Self::new_inline(&[])
+    }
+}
+
+impl ops::Deref for Buf24 {
+    type Target = [u8];
+
+    #[inline(always)]
+    fn deref(&self) -> &[u8] {
+        self.as_bytes()
+    }
+}
+
+// TODO: copy over more methods
---
+++ src/str24.rs
@@ -0,0 +1,438 @@
+use alloc::{borrow::Cow, boxed::Box, string::String, sync::Arc};
+use core::borrow::Borrow;
+use core::cmp::Ordering;
+use core::convert::Infallible;
+use core::mem;
+use core::ops::Deref;
+use core::str::{from_utf8_unchecked, FromStr};
+use core::{fmt, hash, iter};
+
+use crate::buf24::{Buf24, INLINE_CAP};
+
+/// A `Str24` is a string type that has the following properties:
+///
+/// * `size_of::<Str24>() == 24` (therefor `== size_of::<String>()` on 64 bit platforms)
+/// * `size_of::<Option<Str24>>() == size_of::<Str24>()`
+/// * `Clone` is `O(1)`
+/// * Strings are stack-allocated if they are up to 23 bytes long
+/// * If a string does not satisfy the aforementioned conditions, it is heap-allocated
+/// * Additionally, a `Str24` can be explicitly created from a `&'static str` without allocation
+///
+/// Unlike `String`, however, `Str24` is immutable.
+#[derive(Clone, Default, PartialEq, Eq)]
+pub struct Str24(Buf24);
+
+impl Str24 {
+    /// Constructs inline variant of `Str24`.
+    ///
+    /// Panics if `text.len() > 23`.
+    #[inline]
+    pub const fn new_inline(text: &str) -> Str24 {
+        Self(Buf24::new_inline(text.as_bytes()))
+    }
+
+    /// Constructs a `Str24` from a statically allocated string.
+    ///
+    /// This never allocates.
+    #[inline]
+    pub fn new_static(text: &'static str) -> Str24 {
+        Self(Buf24::new_static(text.as_bytes()))
+    }
+
+    pub fn new<T>(text: T) -> Str24
+    where
+        T: AsRef<str>,
+    {
+        Str24(Buf24::new(text.as_ref().as_bytes()))
+    }
+
+    #[inline(always)]
+    pub fn as_str(&self) -> &str {
+        unsafe { from_utf8_unchecked(self.0.as_bytes()) }
+    }
+
+    #[allow(clippy::inherent_to_string_shadow_display)]
+    #[inline(always)]
+    pub fn to_string(&self) -> String {
+        use alloc::borrow::ToOwned;
+
+        self.as_str().to_owned()
+    }
+
+    #[inline(always)]
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    #[inline(always)]
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    #[inline(always)]
+    pub fn is_heap_allocated(&self) -> bool {
+        self.0.is_heap_allocated()
+    }
+
+    fn from_arc(arc: Arc<str>) -> Self {
+        Self(Buf24::from_arc(unsafe { mem::transmute(arc) }))
+    }
+
+    fn from_char_iter<I: iter::Iterator<Item = char>>(mut iter: I) -> Str24 {
+        let (min_size, _) = iter.size_hint();
+        if min_size > INLINE_CAP {
+            let heap: String = iter.collect();
+            let arc: Arc<str> = Arc::from(heap.as_str());
+            return Self::from_arc(arc);
+        }
+        let mut len = 0;
+        let mut buf = [0u8; INLINE_CAP];
+        while let Some(ch) = iter.next() {
+            let size = ch.len_utf8();
+            if size + len > INLINE_CAP {
+                let (min_remaining, _) = iter.size_hint();
+                let mut heap = String::with_capacity(size + len + min_remaining);
+                heap.push_str(core::str::from_utf8(&buf[..len]).unwrap());
+                heap.push(ch);
+                heap.extend(iter);
+                return Self::new(&heap);
+            }
+            ch.encode_utf8(&mut buf[len..]);
+            len += size;
+        }
+        Str24(Buf24::new_inline(&buf[..len]))
+    }
+
+    fn from_str_iter<T>(mut iter: impl Iterator<Item = T>) -> Str24
+    where
+        T: AsRef<str>,
+        String: iter::Extend<T>,
+    {
+        let mut len = 0;
+        let mut buf = [0u8; INLINE_CAP];
+        while let Some(slice) = iter.next() {
+            let slice = slice.as_ref();
+            let size = slice.len();
+            if size + len > INLINE_CAP {
+                let mut heap = String::with_capacity(size + len);
+                heap.push_str(core::str::from_utf8(&buf[..len]).unwrap());
+                heap.push_str(slice);
+                heap.extend(iter);
+                return Str24::new(&heap);
+            }
+            buf[len..][..size].copy_from_slice(slice.as_bytes());
+            len += size;
+        }
+        Str24(Buf24::new_inline(&buf[..len]))
+    }
+}
+
+impl Deref for Str24 {
+    type Target = str;
+
+    #[inline(always)]
+    fn deref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl PartialEq<str> for Str24 {
+    fn eq(&self, other: &str) -> bool {
+        self.as_str() == other
+    }
+}
+
+impl PartialEq<Str24> for str {
+    fn eq(&self, other: &Str24) -> bool {
+        other == self
+    }
+}
+
+impl<'a> PartialEq<&'a str> for Str24 {
+    fn eq(&self, other: &&'a str) -> bool {
+        self == *other
+    }
+}
+
+impl<'a> PartialEq<Str24> for &'a str {
+    fn eq(&self, other: &Str24) -> bool {
+        *self == other
+    }
+}
+
+impl PartialEq<String> for Str24 {
+    fn eq(&self, other: &String) -> bool {
+        self.as_str() == other
+    }
+}
+
+impl PartialEq<Str24> for String {
+    fn eq(&self, other: &Str24) -> bool {
+        other == self
+    }
+}
+
+impl<'a> PartialEq<&'a String> for Str24 {
+    fn eq(&self, other: &&'a String) -> bool {
+        self == *other
+    }
+}
+
+impl<'a> PartialEq<Str24> for &'a String {
+    fn eq(&self, other: &Str24) -> bool {
+        *self == other
+    }
+}
+
+impl Ord for Str24 {
+    fn cmp(&self, other: &Str24) -> Ordering {
+        self.as_str().cmp(other.as_str())
+    }
+}
+
+impl PartialOrd for Str24 {
+    fn partial_cmp(&self, other: &Str24) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl hash::Hash for Str24 {
+    fn hash<H: hash::Hasher>(&self, hasher: &mut H) {
+        self.as_str().hash(hasher);
+    }
+}
+
+impl fmt::Debug for Str24 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(self.as_str(), f)
+    }
+}
+
+impl fmt::Display for Str24 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Display::fmt(self.as_str(), f)
+    }
+}
+
+impl iter::FromIterator<char> for Str24 {
+    fn from_iter<I: iter::IntoIterator<Item = char>>(iter: I) -> Str24 {
+        Self::from_char_iter(iter.into_iter())
+    }
+}
+
+impl iter::FromIterator<String> for Str24 {
+    fn from_iter<I: iter::IntoIterator<Item = String>>(iter: I) -> Str24 {
+        Self::from_str_iter(iter.into_iter())
+    }
+}
+
+impl<'a> iter::FromIterator<&'a String> for Str24 {
+    fn from_iter<I: iter::IntoIterator<Item = &'a String>>(iter: I) -> Str24 {
+        Self::from_str_iter(iter.into_iter().map(|x| x.as_str()))
+    }
+}
+
+impl<'a> iter::FromIterator<&'a str> for Str24 {
+    fn from_iter<I: iter::IntoIterator<Item = &'a str>>(iter: I) -> Str24 {
+        Self::from_str_iter(iter.into_iter())
+    }
+}
+
+impl AsRef<str> for Str24 {
+    #[inline(always)]
+    fn as_ref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl From<&str> for Str24 {
+    #[inline]
+    fn from(s: &str) -> Str24 {
+        Str24::new(s)
+    }
+}
+
+impl From<&mut str> for Str24 {
+    #[inline]
+    fn from(s: &mut str) -> Str24 {
+        Str24::new(s)
+    }
+}
+
+impl From<&String> for Str24 {
+    #[inline]
+    fn from(s: &String) -> Str24 {
+        Str24::new(s)
+    }
+}
+
+impl From<String> for Str24 {
+    #[inline(always)]
+    fn from(text: String) -> Self {
+        Self::new(text)
+    }
+}
+
+impl From<Box<str>> for Str24 {
+    #[inline]
+    fn from(s: Box<str>) -> Str24 {
+        Str24::new(s)
+    }
+}
+
+impl From<Arc<str>> for Str24 {
+    #[inline]
+    fn from(s: Arc<str>) -> Str24 {
+        Self::from_arc(s)
+    }
+}
+
+impl<'a> From<Cow<'a, str>> for Str24 {
+    #[inline]
+    fn from(s: Cow<'a, str>) -> Str24 {
+        Str24::new(s)
+    }
+}
+
+impl From<Str24> for Arc<str> {
+    #[inline(always)]
+    fn from(text: Str24) -> Self {
+        if let Some(arc) = text.0.as_arc() {
+            mem::forget(text);
+            return unsafe { mem::transmute(arc) };
+        }
+        Arc::from(text.as_str())
+    }
+}
+
+impl From<Str24> for String {
+    #[inline(always)]
+    fn from(text: Str24) -> Self {
+        text.as_str().into()
+    }
+}
+
+impl Borrow<str> for Str24 {
+    #[inline(always)]
+    fn borrow(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl FromStr for Str24 {
+    type Err = Infallible;
+
+    #[inline]
+    fn from_str(s: &str) -> Result<Str24, Self::Err> {
+        Ok(Str24::from(s))
+    }
+}
+
+#[cfg(feature = "arbitrary")]
+impl<'a> arbitrary::Arbitrary<'a> for Str24 {
+    fn arbitrary(u: &mut arbitrary::Unstructured<'a>) -> Result<Self, arbitrary::Error> {
+        let s = <&str>::arbitrary(u)?;
+        Ok(Str24::new(s))
+    }
+}
+
+#[cfg(feature = "serde")]
+mod serde {
+    use alloc::string::String;
+    use alloc::vec::Vec;
+    use core::fmt;
+
+    use serde::de::{Deserializer, Error, Unexpected, Visitor};
+
+    use crate::Str24;
+
+    // https://github.com/serde-rs/serde/blob/629802f2abfd1a54a6072992888fea7ca5bc209f/serde/src/private/de.rs#L56-L125
+    fn str24<'de: 'a, 'a, D>(deserializer: D) -> Result<Str24, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct Str24Visitor;
+
+        impl<'a> Visitor<'a> for Str24Visitor {
+            type Value = Str24;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a string")
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: Error,
+            {
+                Ok(Str24::from(v))
+            }
+
+            fn visit_borrowed_str<E>(self, v: &'a str) -> Result<Self::Value, E>
+            where
+                E: Error,
+            {
+                Ok(Str24::from(v))
+            }
+
+            fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+            where
+                E: Error,
+            {
+                Ok(Str24::from(v))
+            }
+
+            fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+            where
+                E: Error,
+            {
+                match core::str::from_utf8(v) {
+                    Ok(s) => Ok(Str24::from(s)),
+                    Err(_) => Err(Error::invalid_value(Unexpected::Bytes(v), &self)),
+                }
+            }
+
+            fn visit_borrowed_bytes<E>(self, v: &'a [u8]) -> Result<Self::Value, E>
+            where
+                E: Error,
+            {
+                match core::str::from_utf8(v) {
+                    Ok(s) => Ok(Str24::from(s)),
+                    Err(_) => Err(Error::invalid_value(Unexpected::Bytes(v), &self)),
+                }
+            }
+
+            fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
+            where
+                E: Error,
+            {
+                match String::from_utf8(v) {
+                    Ok(s) => Ok(Str24::from(s)),
+                    Err(e) => Err(Error::invalid_value(
+                        Unexpected::Bytes(&e.into_bytes()),
+                        &self,
+                    )),
+                }
+            }
+        }
+
+        deserializer.deserialize_str(Str24Visitor)
+    }
+
+    impl serde::Serialize for Str24 {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+        {
+            self.as_str().serialize(serializer)
+        }
+    }
+
+    impl<'de> serde::Deserialize<'de> for Str24 {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            str24(deserializer)
+        }
+    }
+}
